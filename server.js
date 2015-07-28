387f4125e0b18c00025277dc105c15e115330957
Mahendra Kawde <mahendrakawde@gmail.com>
2015-07-28 15:26:06 +0530

    created simple server

---
+++ server.js
@@ -0,0 +1,21 @@
+/*
+var http = require('http');
+
+http.createServer(function(req,res){
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	res.end('Hello!');
+}).listen(1337,'192.168.1.21');
+console.log('Webserver has started');
+*/
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req,res){
+	
+	fs.readFile('index.html', function(err, data){
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(data);
+	});
+}).listen(1337,'192.168.1.21');
+console.log('Webserver has started');
